bd3fd8c96a269a550fab02ac51d17ba1613617eb
guilherme <guibferrarezi@gmail.com>
2026-05-14 20:08:21 -0300

    redirect root to home

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,7 @@
 import { redirect } from "next/navigation";
 
-import { getSessionUser } from "@/lib/session";
-
 export const dynamic = "force-dynamic";
 
 export default async function IndexPage() {
-  const user = await getSessionUser();
-
-  redirect(user ? "/home" : "/login");
+  redirect("/home");
 }
